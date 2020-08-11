fa60e41c11aa4caf8c4b36f4a50709d30bdf80e5
Guiholiiveira <guih.oliiveira@gmail.com>
2020-08-10 22:39:28 -0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var utilitarios = new Utils();
+            utilitarios.Getnull();
+        }
+
+        public class Utils
+        {
+            public void Getnull()
+            {
+                ObjetoX objetox = null;
+                try
+                {
+                    var nome = objetox.Nome;
+                }
+                catch (Exception ex)
+                {
+                    if (ex.GetType() == typeof(NullReferenceException))
+                    {
+                        Console.WriteLine("Gerou uma Exception do tipo NullReferenceException.");
+                    }
+                }
+            }
+        }
+
+        public class ObjetoX
+        {
+            public string Nome { get; set; }
+        }
+    }
+}
